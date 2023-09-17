952c9a1dfb0cf47dc6fd902bfcc549dc3d0cf2ea
gitdevjin <webdev.hjshin@gmail.com>
2023-09-17 10:20:37 -0400

    commit test

--- app/src/About.js
+++ app/src/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <main className='About'>
             <h2>About</h2>
-            <p style={{ marginTop: "1rem" }}>This blog app is a mini react tutorial project.</p>
+            <p style={{ marginTop: "1rem" }}>This blog app is a mini react tutorial project</p>
         </main>
     )
 }
